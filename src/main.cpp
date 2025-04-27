3279446a63d120d93707b31136c3bcaac7275997
Shin Morisawa <tryh4rd3rr@proton.me>
2025-04-27 21:07:05 +0900

    so changed the rand.hpp a lil

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main() {
 	srand(time(NULL));
 	for (int i = 0; i <= 5000; i++) {
-		int n = randint(1, 100);
+		int n = randint(1, 2000);
 		std::cout << "\033[31m" << n << "\033[0m" << std::endl;
 	}
 	return 0;
